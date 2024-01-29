bbd70bffcd23ee34a15ac8c2351dd2efdfe23415
DSAdigamov <adigamov2000@gmail.com>
2024-01-29 12:56:35 +0300

    init

---
+++ src/test/java/ru/adigamov/springbootkafkaexample/SpringBootKafkaExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.adigamov.springbootkafkaexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKafkaExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
